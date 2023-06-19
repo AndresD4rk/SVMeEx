3beb9ff7fa5f8037d9bd52065ac8bd7e291efc60
AndresD4rk <andresfb053@gmail.com>
2023-06-19 11:52:22 -0500

    PrimerPuts

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <base href="" />
+    <title>SVMeEx</title>
+    <meta charset="utf-8" />
+    <meta name="description" content="Supermercado Virtual MercaExpress" />
+    <meta name="keywords" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta property="og:locale" content="es_CO" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Supermercado Virtual MercaExpress" />
+    <meta property="og:url" content="" />
+    <meta property="og:site_name" content="SVMeEx" />
+    <link rel="shortcut icon" href="img\logoMER.webp" />
+    <link rel="stylesheet" href="css/grip.css">
+    <link rel="stylesheet" href="css/base.css">
+</head>
+<body>
+<!-- NAV -->
+<div class="row">
+    <div class="col-4 barnav">Menu</div>
+    <div>Icono</div>
+</div>
+</div>
+</body>
+</html>
