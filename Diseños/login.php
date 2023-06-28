1f221944a602bc0fa2f8ccce1e82685717cebe03
AndresD4rk <andresfb053@gmail.com>
2023-06-27 19:16:44 -0500

    123

---
+++ Diseños/login.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <base href="" />
+    <title>SVMeEx</title>
+    <meta charset="utf-8" />
+    <meta name="description" content="Supermercado Virtual MercaExpress" />
+    <meta name="keywords" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta property="og:locale" content="es_CO" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Supermercado Virtual MercaExpress" />
+    <meta property="og:url" content="" />
+    <meta property="og:site_name" content="SVMeEx" />
+    <link rel="shortcut icon" href="../img/logoMER.webp" />
+    <!-- <link rel="stylesheet" href="css/grip.css?v=1"> -->
+    <link rel="stylesheet" href="../css/cssgrip.css">
+</head>
+
+<body>
+
+    <div class="container">
+    <div>
+            <img src="../img/logoMER.png" alt="logo1" class="col-3">
+        </div><div>
+            <img src="../img/logoMER.png" alt="logo1" class="col-3">
+        </div><div>
+            <img src="../img/logoMER.png" alt="logo1" class="col-3">
+        </div><div>
+            <img src="../img/logoMER.png" alt="logo1" class="col-3">
+        </div><div>
+            <img src="../img/logoMER.png" alt="logo1" class="col-3">
+        </div><div>
+            <img src="../img/logoMER.png" alt="logo1" class="col-3">
+        </div><div>
+            <img src="../img/logoMER.png" alt="logo1" class="col-3">
+        </div><div>
+            <img src="../img/logoMER.png" alt="logo1" class="col-3">
+        </div><div>
+            <img src="../img/logoMER.png" alt="logo1" class="col-3">
+        </div>
+
+
+</body>
+
+</html>
