ff434b2c29abeec85bf9e8ec6e0ddb3af227e7ec
AndresD4rk <andresfb053@gmail.com>
2023-07-01 12:45:29 -0500

    LoginAndRegist

---
+++ regis.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <base href="" />
+  <title>SVMeEx</title>
+  <meta charset="utf-8" />
+  <meta name="description" content="Supermercado Virtual MercaExpress" />
+  <meta name="keywords" content="" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta property="og:locale" content="es_CO" />
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content="Supermercado Virtual MercaExpress" />
+  <meta property="og:url" content="" />
+  <meta property="og:site_name" content="SVMeEx" />
+  <link rel="shortcut icon" href="img/logoMER.webp" />
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/pcss.css">
+</head>
+
+<body>
+<main class="d-flex align-items-center justify-content-center"> 
+  <div id="loginform"> 
+<form id="formid">
+<div  class="mb-4">
+  <h1 class="text-center">MercaExpress</h1>
+  <img src="img/logoMER.png" class="col-2 rounded mx-auto d-block" alt="...">
+</div>
+<div class="col-11 mx-auto">
+<h2 class="text-center">Registro de Usuario</h2>
+<div class="row">
+<div  class="col-6 mb-3 mt-3">
+    <label for="exampleInputEmail1" class="form-label ">Primer Nombre</label>
+    <input type="text" class="form-control" id="PriNom" aria-describedby="emailHelp" placeholder="Ingresa tu primer nombre">
+  </div>
+  <div  class="col-6 mb-3 mt-3">
+    <label for="exampleInputEmail1" class="form-label ">Segundo Nombre</label>
+    <input type="text" class="form-control" id="SegNom" aria-describedby="emailHelp" placeholder="Ingresa tu segundo nombre">
+  </div>
+  <div  class="col-6 mb-3 mt-3">
+    <label for="exampleInputEmail1" class="form-label ">Primer Apellido</label>
+    <input type="text" class="form-control" id="PriNom" aria-describedby="emailHelp" placeholder="Ingresa tu primer apellido">
+  </div>
+  <div  class="col-6 mb-3 mt-3">
+    <label for="exampleInputEmail1" class="form-label ">Segundo Apellido</label>
+    <input type="text" class="form-control" id="SegNom" aria-describedby="emailHelp" placeholder="Ingresa tu segundo apellido">
+  </div>
+</div>
+  <div  class="col-12 mb-3 mt-3">
+    <label for="exampleInputEmail1" class="form-label">Correo Electronico</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingresa el correo">
+    <div id="emailHelp" class="form-text">Su correo electrónico está seguro con nosotros. Nunca lo compartiremos con terceros.</div>
+  </div>
+  <div class="row">
+  <div class="col-6 mb-3">
+    <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Ingresa la contraseña">
+  </div>
+  <div class="col-6 mb-3">
+    <label for="exampleInputPassword1" class="form-label">Verifica tu Contraseña</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Ingresa la nuevamente contraseña">
+  </div>
+  </div>
+  <!-- <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div> -->
+  <div class="row">
+    <div class="col-6 text-start"><button type="submit" class="btn btn-warning">Regresar</button></div>
+    <div class="col-6 text-end   mb-2"><button type="submit" class="btn btn-success">Registrarse</button></div>
+  </div>
+  
+  
+  </div>
+  </div>
+</form>
+</div>
+</main>
+
+</body>
+
+</html>
+
+
+
+
