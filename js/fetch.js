9353cb0c43291ea972ab9fb735448d30f5ea691a
AndresD4rk <andresfb053@gmail.com>
2023-08-14 12:28:10 -0500

    1235

--- js/fetch.js
+++ js/fetch.js
@@ -103,10 +103,106 @@ function mostrarFormulario(formulario) {
                 </div> 
             </form>
       `;
-    }
+    } 
 
     // Mostramos el "offcanvas"
     const offcanvas = new bootstrap.Offcanvas(document.getElementById('offcanvasTop'));
     offcanvas.show();
 }
 
+
+function mostrarFormularioLP(formulario, idProd) {
+    // Obtenemos la referencia al contenedor del formulario
+    const formularioContainer = document.getElementById('formularioContainer');
+
+    // Limpiamos el contenido actual del contenedor
+    formularioContainer.innerHTML = '';
+
+    // Creamos el contenido del formulario según el botón pulsado
+    if (formulario === 1) {
+        formularioContainer.innerHTML = `
+        <!-- Formulario 1 -->
+        <form id="EditPro">
+        <div class="col-11 mx-auto">
+            <h2 class="text-center">Editar de Producto</h2>
+            <div class="row">
+                <!-- CATEGORIA DEL PRODUCTO -->
+                <div class="col-6 mb-3 mt-3">
+                    <label for="exampleInputEmail1" class="form-label ">Categoria</label>
+                    <select class="form-select" aria-label="Default select example" name="SelCat" required>
+                    <option value="">Elija una Categoria</option>
+                        <?php
+                        $sql = $conexion->query("SELECT * 
+                        FROM categoria");
+                        while ($datos = $sql->fetch_array()) {
+                            echo '<option value="' . $datos['idcat'] . ';">' . $datos['nomcat'] . '</option>';
+                        }
+                        ?>
+                    </select>
+                    <button type="button" class="btn btn-warning mt-2" onclick="mostrarFormulario(1)">Agregar Categoria</button>
+                </div>
+                <!-- FAMILIA DEL PRODUCTO -->
+                <div class="col-6 mb-3 mt-3">
+                    <label for="exampleInputEmail1" class="form-label ">Familia</label>
+                    <select class="form-select" aria-label="Default select example" name="SelFam" required>
+                    <option value="">Elija una Familia</option>
+                        <?php
+                        $sql = $conexion->query("SELECT * 
+                        FROM familia");
+                        while ($datos = $sql->fetch_array()) {
+                            echo '<option value="' . $datos['idfam'] . ';">' . $datos['nomfam'] . '</option>';
+                        }
+                        ?>
+                    </select>
+                    <button type="button" class="btn btn-warning mt-2" onclick="mostrarFormulario(2)">Agregar Familia</button>
+                </div>
+                <!-- NOMBRE DEL PRODUCTO -->
+                <div class="col-6 mb-3 mt-3">
+                    <label for="exampleInputEmail1" class="form-label ">Nombre del Producto</label>
+                    <input type="text" class="form-control" name="NomPro" placeholder="Ingresa el nombre del Producto" required>
+                </div>
+                <!-- VALOR DEL PRODUCTO -->
+                <div class="col-6 mb-3 mt-3">
+                    <label for="exampleInputEmail1" class="form-label ">Valor del Producto</label>
+                    <input type="number" class="form-control" name="Precio" placeholder="Ingresa el valor del producto" required>
+                </div>
+                <!-- CANTIDAD DEL PRODUCTO -->            
+                <div class="col-6 mb-3 mt-3">
+                    <label for="exampleInputEmail1" class="form-label">Cantidad del producto</label>
+                    <input type="number" class="form-control" name="CanSto"  placeholder="Ingresa el stock inicial del producto" required>
+                </div>
+                <!-- STOCK MINIMO DEL PRODUCTO -->
+                <div class="col-6 mb-3 mt-3">
+                    <label for="exampleInputEmail1" class="form-label">Cantidad minima del producto</label>
+                    <input type="number" class="form-control" name="MinSto" placeholder="Ingresa el stock minimo del producto" required>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6 text-start"><button class="btn btn-warning">Regresar</button></div>
+                <div class="col-6 text-end   mb-2"><button type="submit" class="btn btn-success">Registrarse</button></div>
+            </div>
+        </div>
+    </form>
+      `;
+    } else if (formulario === 2) {
+        formularioContainer.innerHTML = `
+        <!-- Formulario 2 -->
+            <form id="FamForm">  
+                <div class="col-11 mx-auto">
+                    <h2 class="text-center">Registro de Familia</h2>
+                    <div class="row">
+                        <div class="mb-3 mt-3">
+                            <label class="form-label ">Nombre de la Familia</label>
+                            <input type="text" class="form-control" name="NomFam" placeholder="Ingresa el nombre de la familia">
+                        </div>
+                        <div class="text-end mb-2"><button class="btn btn-success" onclick="enviarFormularioFam()">Registrar</button></div>
+                    </div>
+                </div> 
+            </form>
+      `;
+    } 
+
+    // Mostramos el "offcanvas"
+    const offcanvas = new bootstrap.Offcanvas(document.getElementById('offcanvasTop'));
+    offcanvas.show();
+}
--- pantallas/main.php
+++ pantallas/main.php
@@ -42,44 +42,22 @@
             </div>
             <div class="offcanvas-body">
                 <p>Try scrolling the rest of the page to see this option in action.</p>
-                <div class="row row-cols-1 row-cols-md-2 g-4">
                     <div class="col">
-                        <div class="card">
-                            <img src="../img/producto.webp" class="card-img-top" alt="...">
+                        <div style="background-color:whitesmoke;">                        
                             <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <h5 class="card-title">Productos</h5>
+                                <a href="ListProd.php" class="card-text">Ver productos existentes</A>
                             </div>
                         </div>
                     </div>
                     <div class="col">
-                        <div class="card">
-                            <img src="../img/producto.webp" class="card-img-top" alt="...">
+                        <div style="background-color:whitesmoke;">                        
                             <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <h5 class="card-title">Usuarios</h5>
+                                <a href="ListUsu.php" class="card-text">Ver Usuarios existentes</A>
                             </div>
                         </div>
                     </div>
-                    <div class="col">
-                        <div class="card">
-                            <img src="../img/producto.webp" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card">
-                            <img src="../img/producto.webp" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
         <!-- Fin Menu LATERAL -->
