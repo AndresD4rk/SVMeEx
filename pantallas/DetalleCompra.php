698e19a6267b98eea94f56edb941e6fc23454275
AndresD4rk <andresfb053@gmail.com>
2023-09-08 13:15:12 -0500

    fixmaxruta

--- pantallas/DetalleCompra.php
+++ pantallas/DetalleCompra.php
@@ -185,10 +185,9 @@ include "../procesos/conexion.php";
             <div class="container-fluid">
                 <div class="row">
                     <div class=" col-xl-6 col-12">
-                    <div class="progress">
-  <div id="barentrega" class="progress-bar" role="progressbar" aria-label="Basic example" style="width: 0%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-</div>
-
+                        <div class="progress m-2">
+                            <div id="barentrega" class="progress-bar" role="progressbar" aria-label="Basic example" style="width: 0%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
                         <table class="table table-bordered" id="dataTable-1">
                             <thead>
                                 <tr style="text-align: center;">
@@ -247,20 +246,16 @@ include "../procesos/conexion.php";
 </script>
 
 <script>
-    
-
-
-
-
-
-
     //alert(locations);
     var map, directionsService, directionsRenderer, latitud, longitud;
 
     function initMap() {
         // Crear el mapa
         map = new google.maps.Map(document.getElementById('map'), {
-            center: { lat: 0, lng: 0 },
+            center: {
+                lat: 0,
+                lng: 0
+            },
             zoom: 8
         });
 
@@ -280,74 +275,75 @@ include "../procesos/conexion.php";
         directionsRenderer.setMap(map);
         //dibujarRuta();
         var locations;
-    $(document).ready(function() {
-        // Función para actualizar el contenido
-        function actualizarContenido() {
-            $.ajax({
-                url: '../procesos/actualizar_localizacion.php?entrega=' + <?php echo number_format($ident) ?>, // Archivo PHP que actualiza la localización
-                success: function(data) {
-                    if (data && data.length > 0) {
-                        locations = data;
-                        console.log(locations);
-                        //console.log(locations[0].latrep); 
-                        dibujarRuta()
+        $(document).ready(function() {
+            // Función para actualizar el contenido
+            function actualizarContenido() {
+                $.ajax({
+                    url: '../procesos/actualizar_localizacion.php?entrega=' + <?php echo number_format($ident) ?>, // Archivo PHP que actualiza la localización
+                    success: function(data) {
+                        if (data && data.length > 0) {
+                            locations = data;
+                            console.log(locations);
+                            //console.log(locations[0].latrep); 
+                            dibujarRuta()
+                        }
+                    },
+                    error: function(xhr, status, error) {
+                        console.error('Error en la solicitud AJAX:', error);
+                    },
+                    complete: function() {
+                        // Vuelve a llamar a la función después de cierto tiempo
+                        setTimeout(actualizarContenido, 30000); // Actualiza cada 5 segundos (ajusta el tiempo según tus necesidades)
                     }
-                },
-                error: function(xhr, status, error) {
-                    console.error('Error en la solicitud AJAX:', error);
-                },
-                complete: function() {
-                    // Vuelve a llamar a la función después de cierto tiempo
-                    setTimeout(actualizarContenido, 30000); // Actualiza cada 5 segundos (ajusta el tiempo según tus necesidades)
-                }
-            });
-        }
-        actualizarContenido();
-        // Inicia la actualización inicial del contenido
-       
-
-        function dibujarRuta() {
-        // Configurar la solicitud de ruta
-        // var supermark = {lat:6.4689837,lng:-73.2631887};
-        var puntoA = new google.maps.LatLng(6.4689837, -73.2631887);
-        // var domi = {lat: parseFloat(locations[0].latrep),lng: parseFloat(locations[0].lonrep)};
-        var puntoC = new google.maps.LatLng(parseFloat(locations[0].latrep), parseFloat(locations[0].lonrep));
-        // var entrega = {lat: parseFloat(locations[0].latent),lng: parseFloat(locations[0].lonent)};
-        var puntoB = new google.maps.LatLng(parseFloat(locations[0].latent), parseFloat(locations[0].lonent));
-        var request = {
-            origin: {
-                lat: parseFloat(locations[0].latrep),
-                lng: parseFloat(locations[0].lonrep)
-            }, // Coordenadas del origen
-            destination: {
-                lat: parseFloat(locations[0].latent),
-                lng: parseFloat(locations[0].lonent)
-            }, // Coordenadas del destino
-            travelMode: google.maps.TravelMode.DRIVING // Modo de transporte (puedes cambiarlo según tus necesidades)
-        };
-        //alert(locations + "Hola")
-        // Obtener la ruta utilizando el servicio de direcciones
-        directionsService.route(request, function(result, status) {
-            if (status === google.maps.DirectionsStatus.OK) {
-                // Mostrar la ruta en el mapa
-                directionsRenderer.setDirections(result);
-                var distancia_total = google.maps.geometry.spherical.computeDistanceBetween(puntoA,puntoB);
-                var distancia_recorrida = google.maps.geometry.spherical.computeDistanceBetween(puntoA, puntoC);
-                var progreso = (distancia_recorrida / distancia_total) * 100;
-                console.log('Progreso de entrega: '+progreso.toFixed(2) + '%');
-                var barentrega = document.getElementById('barentrega');
-                barentrega.style.width = progreso.toFixed(2) + '%';
-                
-            } else {
-                // Manejar el caso de error
-                window.alert('No se pudo trazar la ruta: ' + status);
+                });
             }
+            actualizarContenido();
+            // Inicia la actualización inicial del contenido
+
+
+            function dibujarRuta() {
+                // Configurar la solicitud de ruta
+                // var supermark = {lat:6.4689837,lng:-73.2631887};
+                var puntoA = new google.maps.LatLng(6.4689837, -73.2631887);
+                // var domi = {lat: parseFloat(locations[0].latrep),lng: parseFloat(locations[0].lonrep)};
+                var puntoC = new google.maps.LatLng(parseFloat(locations[0].latrep), parseFloat(locations[0].lonrep));
+                // var entrega = {lat: parseFloat(locations[0].latent),lng: parseFloat(locations[0].lonent)};
+                var puntoB = new google.maps.LatLng(parseFloat(locations[0].latent), parseFloat(locations[0].lonent));
+                var request = {
+                    origin: {
+                        lat: parseFloat(locations[0].latrep),
+                        lng: parseFloat(locations[0].lonrep)
+                    }, // Coordenadas del origen
+                    destination: {
+                        lat: parseFloat(locations[0].latent),
+                        lng: parseFloat(locations[0].lonent)
+                    }, // Coordenadas del destino
+                    travelMode: google.maps.TravelMode.DRIVING // Modo de transporte (puedes cambiarlo según tus necesidades)
+                };
+                //alert(locations + "Hola")
+                // Obtener la ruta utilizando el servicio de direcciones
+                directionsService.route(request, function(result, status) {
+                    if (status === google.maps.DirectionsStatus.OK) {
+                        // Mostrar la ruta en el mapa
+                        directionsRenderer.setDirections(result);
+                        var distancia_total = google.maps.geometry.spherical.computeDistanceBetween(puntoA, puntoB);
+                        var distancia_recorrida = google.maps.geometry.spherical.computeDistanceBetween(puntoC, puntoB);
+                        var fixdistancia = distancia_total - distancia_recorrida;
+                        var progreso = (fixdistancia * 100) / distancia_total;
+                        console.log('Progreso de entrega: ' + progreso.toFixed(2) + '%');
+                        var barentrega = document.getElementById('barentrega');
+                        barentrega.style.width = progreso + '%';
+
+                    } else {
+                        // Manejar el caso de error
+                        window.alert('No se pudo trazar la ruta: ' + status);
+                    }
+                });
+
+            }
+
         });
 
-    }
-
-    });
-        
     }
 </script>
 
